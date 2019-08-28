27cc6037ddc00be436347cfd9303ec1bd58518d9
Maurice <MauriceNgassa.Happi@antrago.de>
2019-08-28 07:45:36 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, of, from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'observable';
+
+// Create simple observable that emits three values
+myObservable = of(1, 2, 3);
+
+// Create observer object
+  myObserver = this.myObservable.subscribe({
+  next: x => console.log('Observer got a next value: ' + x),
+  error: err => console.error('Observer got an error: ' + err),
+  complete: () => console.log('Observer got a complete notification'),
+});
+
+
 }
